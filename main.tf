b2dafd0b8dd966c929a7f884af558139207f1871
akshaykolhe1989 <akshaykolhe1809@gmail.com>
2022-02-27 21:24:01 +0000

    Updated pipeline file

--- main.tf
+++ main.tf
@@ -3,6 +3,18 @@ provider "azurerm" {
     features {}  
 }
 
+terraform {
+  backend "azurerm" {
+    resource_group_name = "tf_rg_blobstore"
+    storage_account_name = "tfstorageakdocpro001"
+    container_name = "tfstate"
+    key = "terraform.tfstate"
+    
+
+    
+  }
+}
+
 resource "azurerm_resource_group" "tf_test" {
   name = "tfmainrg"
   location = "Australia East"
